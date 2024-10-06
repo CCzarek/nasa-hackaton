410d4d9bca77524cb374a0250041f26c1271ad13
Kuba Maciążek <126525650+JakMac02@users.noreply.github.com>
2024-10-06 15:36:19 +0200

    Update main.py

--- read-seismic-data-jm/main.py
+++ read-seismic-data-jm/main.py
@@ -1,7 +1,1 @@
-def print_hi():
-    # Use a breakpoint in the code line below to debug your script.
-    print('Hello World!')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-if __name__ == '__main__':
-    print_hi()
